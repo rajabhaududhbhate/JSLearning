f740eb95e75190c526d8a7dbb4d0a6db4a61d6e1
rajabhaududhbhate <rajabhaududhbhate@gmail.com>
2023-02-16 08:46:08 +0530

    javaScript1lecture

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,22 @@
+<<<<<<< Updated upstream
 console.log("Raju D Dudhbhate ");
+=======
+console.log("Raju Dudhbhate");
+console.log(31)
+
+var fullName = "Raju Dudhbhate";
+console.log(fullName);
+console.log(fullName);
+
+var city; // Variable Declaration
+console.log(city);
+city = "Pune";// Variable Initialization
+console.log(city);
+city = "Surat"; // Variable Update
+console.log(city);
+
+var age = "31";  // Variable declaration and initialization in the same line
+age = 32;
+age = 33;
+console.log(age);
+>>>>>>> Stashed changes
