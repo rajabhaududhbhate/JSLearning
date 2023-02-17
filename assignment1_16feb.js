16ba912d2f106fc1de536dc07585a5f72af30721
rajabhaududhbhate <rajabhaududhbhate@gmail.com>
2023-02-17 20:16:24 +0530

    dataype_assignment
    dataype_assignment

---
+++ assignment1_16feb.js
@@ -0,0 +1,55 @@
+var pincode = 411041;
+console.log("Pin Code:", pincode, typeof pincode);
+
+var myName = "Raju"
+console.log("My Name is:", myName, typeof myName);
+
+var isWorking = true;
+console.log("Working:", isWorking, typeof isWorking);
+
+
+console.log("**************** 2 Assignment ****************");
+
+var bankName;
+console.log("Bank Name", bankName, typeof bankName);
+
+bankName = "SBI"
+console.log("Bank Name", bankName, typeof bankName);
+
+var result = "75%";
+
+console.log("Marks", result, typeof result);
+
+var is_Working;
+console.log("Is Working", is_Working, typeof is_Working);
+
+is_Working = true;
+
+console.log("Is Working", is_Working, typeof is_Working);
+
+
+var totalCount = "Hundred and Seven";
+
+console.log("Total Count:", totalCount, typeof totalCount);
+
+totalCount = 107;
+
+console.log("Total Count Udated", totalCount, typeof totalCount);
+
+var firstName = "Sweety";
+var secondName = "Cutie";
+
+console.log("first Name", firstName, "Second Name", secondName);
+var thirdName = firstName;
+firstName = secondName;
+
+
+
+console.log("first Name and second name is same", firstName, secondName);
+
+console.log("First Name is", thirdName );
+
+
+
+
+
