f92d4802c46f82dce5370089be0f465c1e6082ef
rajabhaududhbhate <rajabhaududhbhate@gmail.com>
2023-02-22 12:27:54 +0530

    string template and operator
    string template and operator

---
+++ 04_stringTemplate.js
@@ -0,0 +1,10 @@
+console.log(`you are "champ" man`);
+console.log(`  `);
+console.log(`you are "champ" man`);
+
+var firstName = "Raju";
+var lastName = "Dudhbhate";
+var city = "Pune";
+console.log("My first name is:", firstName, "My last name is:", lastName, "City:", city );
+
+console.log(`My first name ${firstName} My last name is ${lastName} City: ${city}`);
---
+++ 05_Operator.js
@@ -0,0 +1,57 @@
+console.log(`=========== Arithmetic Operator ============`);
+var num1 = 10;
+var num2 = 2;
+var result = num1 + num2;  // Addition
+console.log(`Addition is ${result}`);
+
+var result = num1 - num2; // substraction
+console.log(`sbustration is ${result}`);
+
+var result = num1 * num2; // multiplication
+console.log(`multiplication is ${result}`);
+
+
+var result = num1 ** num2; // Exponential
+console.log(`Exponential is ${result}`);
+
+var result = num1 / 3; // Division
+console.log(`Division is ${result}`);
+
+var result = num1 % 3; // Modulus
+console.log(`Reminder is ${result}`);
+
+var result = num1 ** 3; // Exponential
+console.log(`Exponential is ${result}`);
+
+var num = 10; // compund addition +=
+num +=20 //num = num + 20
+console.log(`compund addition += ${num}`);
+
+var num3 = "10";
+var num4 = 10;
+console.log(num3==num4);
+
+var num3 = "10";
+var num4 = 10;
+console.log(num3===num4);
+
+var num3 = "10";
+var num4 = 10;
+console.log(num3!==num4); //Not equal
+
+console.log(10 > 20); // Grater than
+console.log(20 < 10); // Grater than
+
+console.log(10 < 10); // Less than
+
+
+var marks = 35; //greater than equal
+console.log(marks >= 35); // 
+
+// Is marks greater than equal 35
+
+
+var marks = 35; //greater than equal
+console.log(marks <= 35); // 
+
+// Is marks greater than equal 35
