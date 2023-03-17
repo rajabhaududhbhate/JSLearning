290baa15c0417ec85baccd6bab607c115b8923af
rajabhaududhbhate <rajabhaududhbhate@gmail.com>
2023-03-17 08:56:36 +0530

    closer, higherobject,firstprime

--- 13_hosting.js
+++ 13_hosting.js
@@ -14,3 +14,6 @@ let greet = function(){
     console.log("Good Morning");
 }
 greet();
+
+
+inner();
---
+++ 15_spradOperator.js
@@ -0,0 +1,6 @@
+// ... Rest Parameter
+function show(arg1, arg2, ...arg){
+    console.log(arg1, arg2, arg);
+
+}
+show(3, 4, 6, 8, 9);
